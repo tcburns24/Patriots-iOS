ad12c98963c63058acde74d923381b73abcd2d80
Thomas Burns <tom@themighty.com>
2019-05-05 15:56:06 -0400

    Completes home page visuals

---
+++ Patriots2/PatsColors.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Patriots2
+//
+//  Created by Thomas Burns on 5/5/19.
+//  Copyright © 2019 Thomas Burns. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct PatsColors {
+    static let patsBlue   = UIColor(red: 0.0/255.0, green: 34.0/255.0, blue: 68.0/255.0, alpha: 1.0)
+    static let patsRed    = UIColor(red: 198.0/255.0, green: 12.0/255.0, blue: 48.0/255.0, alpha: 1.0)
+    static let patsSilver = UIColor(red: 176.0/255.0, green: 183.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+}
--- Patriots2/ViewController.swift
+++ Patriots2/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.setGradientBackground(colorOne: PatsColors.patsBlue, colorTwo: UIColor(red: 10/255, green: 47/255, blue: 109/255, alpha: 1.0) )
+        
+        self.title = "New England Patriots | 6 Rings"
     }
 
 
