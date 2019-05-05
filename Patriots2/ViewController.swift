ce489c9c42151631338d81e062b3f4fb58fb93e9
Thomas Burns <tom@themighty.com>
2019-05-05 12:15:01 -0400

    Initial Commit

---
+++ Patriots2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Patriots2
+//
+//  Created by Thomas Burns on 5/5/19.
+//  Copyright © 2019 Thomas Burns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
